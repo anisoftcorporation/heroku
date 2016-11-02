b8d97ae37d48971235dd36699d9f0e811d34782b
abhattacharya <abhattacharya@paypal.com>
2016-11-01 23:46:06 -0700

    More routes

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!'+process.env.PORT);
 });
+app.get('/login', function (req, res) {
+  res.send('I will take you to Login screen!'+process.env.PORT);
+});
+app.get('/dashboard', function (req, res) {
+  res.send('I Will take you to dashboard!'+process.env.PORT);
+});
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port!');
